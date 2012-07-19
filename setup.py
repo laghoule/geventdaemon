001102b2b62bad61ea0eaa3c99d33432c6ffa901
pgauthier <pgauthier@debian.(none)>
2012-07-19 10:08:32 -0400

    Packaging should work fine now with: python setup.py --command-packages=stdeb.command debianize

--- setup.py
+++ setup.py
@@ -1,14 +1,11 @@
-from setuptools import setup, find_packages
-
-files = ["geventdaemon.py"]
+from setuptools import setup
 
 setup(name="geventdaemon",
       version="1.0",
       author="Antonin Amand",
       author_email="antonin.amand@gmail.com",
       description="gevent daemonizer",
-      packages = ['geventdaemon'],
-      package_data = {'package' : files },
+      py_modules=['geventdaemon'],
       zip_safe=False,
       install_requires=[
           'gevent',
